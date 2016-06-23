5bf8b6e00d590a38acaca6e9bbacf64a43d615e8
lihaijian <lihaijian@ygsoft.com>
2016-06-23 17:10:46 +0800

    no message

--- Linkage/controller/mine/advice/AdviceViewController.m
+++ Linkage/controller/mine/advice/AdviceViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "AdviceViewController.h"
+#import "BFPaperButton.h"
 #import <IQKeyboardManager/KeyboardManager.h>
 #import <XLForm/XLFormTextView.h>
 @interface AdviceViewController ()
@@ -24,22 +25,57 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = @"建议";
+
     UIBarButtonItem *rightItem = [[UIBarButtonItem alloc] initWithTitle:@"确定" style:UIBarButtonItemStylePlain target:self action:@selector(doneAction)];
     self.navigationItem.rightBarButtonItem = rightItem;
-    self.view.backgroundColor = [UIColor whiteColor];
+    self.view.backgroundColor = BackgroundColor;
     
     [self.view addSubview:self.textView];
     [self.textView makeConstraints:^(MASConstraintMaker *make) {
-        make.edges.equalTo(self.view).insets(UIEdgeInsetsMake(5, 8, 5, 8));
+        make.top.equalTo(self.view.top).offset(12);
+        make.left.equalTo(self.view.left).offset(12);
+        make.right.equalTo(self.view.right).offset(-12);
+        make.height.equalTo(@(200));
+    }];
+    
+    UIView *bottomView = [UIView new];
+    bottomView.backgroundColor = BackgroundColor;
+    [self.view addSubview:bottomView];
+    [bottomView makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.view.left);
+        make.right.equalTo(self.view.right);
+        make.bottom.equalTo(self.view.bottom);
+        make.height.equalTo(54);
+    }];
+    
+    BFPaperButton *button = ({
+        BFPaperButton *button = [[BFPaperButton alloc]initWithRaised:NO];
+        button.cornerRadius = 4;
+        [button setBackgroundImage:ButtonBgImage forState:UIControlStateNormal];
+        [button setBackgroundImage:ButtonBgImage forState:UIControlStateHighlighted];
+        [button setBackgroundImage:ButtonDisableBgImage forState:UIControlStateDisabled];
+        [button setTitleFont:[UIFont fontWithName:@"HelveticaNeue-Light" size:20.f]];
+        [button setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        [button setTitleColor:[UIColor grayColor] forState:UIControlStateDisabled];
+        NSAttributedString *title = [[NSAttributedString alloc]initWithString:@"电话" attributes:@{NSForegroundColorAttributeName:[UIColor whiteColor],NSFontAttributeName:[UIFont systemFontOfSize:20]}];
+        [button setAttributedTitle:title forState:UIControlStateNormal];
+        [button addTarget:self action:@selector(forgetAction:) forControlEvents:UIControlEventTouchUpInside];
+        button;
+    });
+    [bottomView addSubview:button];
+    [button makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(bottomView.left).offset(10);
+        make.right.equalTo(bottomView.right).offset(-10);
+        make.top.equalTo(bottomView.top).offset(5);
+        make.height.equalTo(@44);
     }];
 }
+
 
 -(void)viewDidAppear:(BOOL)animated
 {
     [super viewDidAppear:animated];
-    [self.textView becomeFirstResponder];
 }
-
 
 - (void)doneAction
 {
