aa60e6f1d4c0c39a7d5edc7ce3040a9b775bb1e9
near2sea <sanan.li@qq.com>
2016-06-26 23:09:40 +0800

    1

--- Linkage/controller/message/model/Message.m
+++ Linkage/controller/message/model/Message.m
@@ -34,4 +34,34 @@
     return keyDic;
 }
 
+#pragma mark - 属性转换
++(NSValueTransformer *)typeJSONTransformer
+{
+    return [MTLValueTransformer transformerUsingForwardBlock:^id(id value, BOOL *success, NSError **error) {
+        if ([value isKindOfClass:[NSString class]]) {
+            return @([value intValue]);
+        }else if ([value isKindOfClass:[NSNumber class]]){
+            return value;
+        }else{
+            return @(0);
+        }
+    } reverseBlock:^id(id value, BOOL *success, NSError **error) {
+        return value;
+    }];
+}
+
++ (NSValueTransformer *)createTimeJSONTransformer
+{
+    return [MTLValueTransformer transformerUsingForwardBlock:^id(id dateString, BOOL *success, NSError *__autoreleasing *error) {
+        if ([dateString isKindOfClass:[NSDate class]]) {
+            return dateString;
+        }else{
+            return [NSDate dateWithTimeIntervalSince1970:[dateString doubleValue]];
+        }
+    } reverseBlock:^id(NSDate *date, BOOL *success, NSError *__autoreleasing *error) {
+        long long dTime = [[NSNumber numberWithDouble:[date timeIntervalSince1970]] longLongValue];
+        return @(dTime);
+    }];
+}
+
 @end
