edfc5862ded2d5287e9ea7b128c00a1fc357a9ef
lihaijian <lihaijian@ygsoft.com>
2017-01-03 13:48:21 +0800

    no message

---
+++ Vcode/controller/rank/model/VCRankModel.h
@@ -0,0 +1,27 @@
+//
+//  VCRankModel.h
+//  Linkage
+//
+//  Created by Mac mini on 2017/1/3.
+//  Copyright © 2017年 LA. All rights reserved.
+//
+
+#import <CoreData/CoreData.h>
+
+@interface VCRankModel : NSManagedObject
+@property (nullable, nonatomic, retain) NSString *gid;
+@property (nullable, nonatomic, retain) NSString *url;
+@property (nullable, nonatomic, retain) NSString *name;
+@property (nullable, nonatomic, retain) NSString *introduction;
+@property (nullable, nonatomic, retain) NSString *country;
+@property (nullable, nonatomic, retain) NSString *countryCode;
+@property (nullable, nonatomic, retain) NSString *categoryCode;
+@property (nullable, nonatomic, retain) NSString *subCategory;
+@property (nullable, nonatomic, retain) NSString *subCategoryCode;
+@property (nullable, nonatomic, retain) NSString *rank;
+@property (nullable, nonatomic, retain) NSDate   *createdDate;
+@property (nullable, nonatomic, retain) NSString *userId;
+@property (nullable, nonatomic, retain) NSNumber *visiteCount;
+@property (nullable, nonatomic, retain) NSNumber *favorCount;
+@property (nullable, nonatomic, retain) NSNumber *likeCount;
+@end
---
+++ Vcode/controller/rank/model/VCRankModel.m
@@ -0,0 +1,27 @@
+//
+//  VCRankModel.m
+//  Linkage
+//
+//  Created by Mac mini on 2017/1/3.
+//  Copyright © 2017年 LA. All rights reserved.
+//
+
+#import "VCRankModel.h"
+
+@implementation VCRankModel
+@dynamic gid;
+@dynamic url;
+@dynamic name;
+@dynamic introduction;
+@dynamic country;
+@dynamic countryCode;
+@dynamic categoryCode;
+@dynamic subCategory;
+@dynamic subCategoryCode;
+@dynamic rank;
+@dynamic createdDate;
+@dynamic userId;
+@dynamic visiteCount;
+@dynamic favorCount;
+@dynamic likeCount;
+@end
