099f2fa07ff7a80ecf48b3557ca12bdcd98dd994
near2sea <sanan.li@qq.com>
2017-01-04 23:28:27 +0800

    1

---
+++ Vcode/controller/mine/setting/VCSettingViewController.h
@@ -0,0 +1,13 @@
+//
+//  VCSettingViewController.h
+//  Linkage
+//
+//  Created by lihaijian on 2017/1/4.
+//  Copyright © 2017年 LA. All rights reserved.
+//
+
+#import <XLForm/XLForm.h>
+
+@interface VCSettingViewController : XLFormViewController
+
+@end
---
+++ Vcode/controller/mine/setting/VCSettingViewController.m
@@ -0,0 +1,83 @@
+//
+//  VCSettingViewController.m
+//  Linkage
+//
+//  Created by lihaijian on 2017/1/4.
+//  Copyright © 2017年 LA. All rights reserved.
+//
+
+#import "VCSettingViewController.h"
+
+#define RowUI [row.cellConfig setObject:@(NSTextAlignmentLeft) forKey:@"textLabel.textAlignment"];\
+row.cellStyle = UITableViewCellStyleValue1;
+#define RowPlaceHolderUI(str) [row.cellConfigAtConfigure setObject:str forKey:@"detailTextLabel.text"];
+#define RowAccessoryUI [row.cellConfig setObject:@(UITableViewCellAccessoryDisclosureIndicator) forKey:@"accessoryType"];
+@interface VCSettingViewController ()
+
+@end
+
+@implementation VCSettingViewController
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        [self initializeForm];
+    }
+    return self;
+}
+
+
+- (void)initializeForm
+{
+    WeakSelf
+    XLFormDescriptor * form;
+    XLFormSectionDescriptor * section;
+    XLFormRowDescriptor * row;
+    
+    form = [XLFormDescriptor formDescriptorWithTitle:@"我的"];
+    
+    section = [XLFormSectionDescriptor formSection];
+    [form addFormSection:section];
+
+    
+    self.form = form;
+}
+
+-(void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.tableView.sectionHeaderHeight = 20;
+    self.tableView.sectionFooterHeight = 0;
+    self.tableView.tableHeaderView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, IPHONE_WIDTH, 18)];
+}
+
+
+#pragma mark - XLFormDescriptorDelegate
+-(void)formRowDescriptorValueHasChanged:(XLFormRowDescriptor *)formRow oldValue:(id)oldValue newValue:(id)newValue
+{
+}
+
+
+
+-(void)clearCacheAction:(XLFormRowDescriptor *)row
+{
+    [self deselectFormRow:row];
+    [self reloadFormRow:row];
+}
+
+-(void)shareAction:(XLFormRowDescriptor *)row
+{
+    [self deselectFormRow:row];
+}
+
+-(void)updateAction:(XLFormRowDescriptor *)row
+{
+    [self deselectFormRow:row];
+}
+
+-(void)aboutUSAction:(XLFormRowDescriptor *)row
+{
+    [self deselectFormRow:row];
+}
+
+@end
