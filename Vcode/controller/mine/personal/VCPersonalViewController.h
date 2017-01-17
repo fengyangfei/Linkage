17c96031e8682bb54f78a567d634c0be25bb3094
lihaijian <lihaijian@ygsoft.com>
2017-01-17 17:02:47 +0800

    1

---
+++ Vcode/controller/mine/personal/VCPersonalViewController.h
@@ -0,0 +1,13 @@
+//
+//  VCPersonalViewController.h
+//  Linkage
+//
+//  Created by Mac mini on 2017/1/17.
+//  Copyright © 2017年 LA. All rights reserved.
+//
+
+#import <XLForm/XLForm.h>
+
+@interface VCPersonalViewController : XLFormViewController
+
+@end
---
+++ Vcode/controller/mine/personal/VCPersonalViewController.m
@@ -0,0 +1,64 @@
+//
+//  VCPersonalViewController.m
+//  Linkage
+//
+//  Created by Mac mini on 2017/1/17.
+//  Copyright © 2017年 LA. All rights reserved.
+//
+
+#import "VCPersonalViewController.h"
+
+@interface VCPersonalViewController ()
+
+@end
+
+@implementation VCPersonalViewController
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        [self initializeForm];
+    }
+    return self;
+}
+
+
+- (void)initializeForm
+{
+    XLFormDescriptor * form;
+    XLFormSectionDescriptor * section;
+    XLFormRowDescriptor * row;
+    
+    form = [XLFormDescriptor formDescriptorWithTitle:VCThemeString(@"setting")];
+    
+    section = [XLFormSectionDescriptor formSection];
+    [form addFormSection:section];
+    
+    row = [XLFormRowDescriptor formRowDescriptorWithTag:nil rowType:XLFormRowDescriptorTypeText];
+    [section addFormRow:row];
+    
+    row = [XLFormRowDescriptor formRowDescriptorWithTag:nil rowType:XLFormRowDescriptorTypeText];
+    [section addFormRow:row];
+    
+    row = [XLFormRowDescriptor formRowDescriptorWithTag:nil rowType:XLFormRowDescriptorTypeText];
+    [section addFormRow:row];
+    
+    row = [XLFormRowDescriptor formRowDescriptorWithTag:nil rowType:XLFormRowDescriptorTypeText];
+    [section addFormRow:row];
+    
+    row = [XLFormRowDescriptor formRowDescriptorWithTag:nil rowType:XLFormRowDescriptorTypeText];
+    [section addFormRow:row];
+    
+    self.form = form;
+}
+
+-(void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.tableView.sectionHeaderHeight = 20;
+    self.tableView.sectionFooterHeight = 0;
+    self.tableView.tableHeaderView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, IPHONE_WIDTH, 18)];
+}
+
+@end
